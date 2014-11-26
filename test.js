9b7573d7309af820aa78f507c4342168d9db4a7d
Tejas Manohar <me@tejasmanohar.com>
2014-11-26 03:16:23 -0600

    test passing

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ var test = require('ava');
 var isPhoneNumber = require('./');
 
 test(function (t) {
-  t.assert(isPhoneNumber('614-223-4244'));
-  t.assert(isPhoneNumber('(842) 984-153'));
-  t.assert(isPhoneNumber('123 434 3584'));
-  t.assert(isPhoneNumber('523.753.9082'));
+  t.assert(isPhoneNumber('123-456-7890'));
+  t.assert(isPhoneNumber('(123) 456-7890'));
+  t.assert(isPhoneNumber('123 456 7890'));
+  t.assert(isPhoneNumber('123.456.7890'));
   t.assert(isPhoneNumber('+91 (123) 456-7890'));
-  t.assert(!isUrl('5222-222-333'));
+  t.assert(!isPhoneNumber('5222-222-333'));
   t.end();
 });
