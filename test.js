63b81d331ce6c70575e540531cff4ccca53f58c6
Tejas Manohar <me@tejasmanohar.com>
2014-12-07 14:45:45 -0600

    version up, new test

--- test.js
+++ test.js
@@ -10,5 +10,6 @@ test(function (t) {
   t.assert(isPhoneNumber('123.456.7890'));
   t.assert(isPhoneNumber('+91 (123) 456-7890'));
   t.assert(!isPhoneNumber('5222-222-333'));
+  t.assert(!isPhoneNumber('lol lol-loll'))
   t.end();
 });
